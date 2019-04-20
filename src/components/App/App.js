c6aa9bfcf60e087a95dde8d6972eb0dc33750e4b
wabens <waltkbenson@gmail.com>
2019-04-20 13:33:43 -0500

    test data in data.modules, exported to app, in console.log

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const incubatorData = require('../../data.modules/incubatorData')
+
+
 
 class App extends Component {
+
+  componentDidMount(){
+    console.log(`incubator data `, incubatorData);
+  }
   render() {
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  
       </div>
     );
   }
---
+++ src/components/Table/Table.js
@@ -0,0 +1,3 @@
+const incubatorData = require('../data.modules/incubatorData')
+
+console.log(`incubator data `, incubatorData);
