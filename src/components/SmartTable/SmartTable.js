fc19e9ed166c94d09460b756405f74b789800464
wabens <waltkbenson@gmail.com>
2019-04-20 18:08:28 -0500

    fixed header

--- src/components/SmartTable/SmartTable.js
+++ src/components/SmartTable/SmartTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 //import SmartDataTable from 'react-smart-data-table'
 import { Column, Table, AutoSizer } from 'react-virtualized';
+import './SmartTable.css'
 import 'react-virtualized/styles.css'; // only needs to be imported once
 
 const incubatorData = require('../../data.modules/incubatorData')
@@ -31,17 +32,18 @@ class SmartTable extends Component {
           <Table
             height={1000}
             width={1000}
-            headerHeight={30}
+            headerHeight={50}
             rowCount={incubatorData.length}
             rowGetter={({ index }) => incubatorData[index]}
             rowHeight={30}
+            //headerClassName={'fixedHeader'}
           >
           {columnNames.map(key => 
             <Column
 
               label= {key}
               dataKey={key}
-              width={100}
+              width={50}
             /> 
           )}
 
